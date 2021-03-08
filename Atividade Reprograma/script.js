ec31b5efa87fc02d9907fb9d625e17dd92db74f8
nathmelop <nathaliapmelo@hotmail.com>
2021-03-08 15:43:59 -0300

    up

--- Atividade Reprograma/script.js
+++ Atividade Reprograma/script.js
@@ -1,7 +1,7 @@
 document.getElementById("botao-enviar").addEventListener("click", validaFormulario)
 
 function validaFormulario() {
-    if (document.getElementById("nome").value != "" && document.getElementById("email").value != "") {
+    if (document.getElementById("nome").value != "" && document.getElementById("email").value != "" && document.getElementById("telefone").value != "") {
         alert("Prontinho!!Você receberá as novidades por email!!")
     } else {
         alert("preencha todos os campos!")
